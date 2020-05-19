bdd842c4958b1163a2285d812b91428e7e0c52cb
gdgagan696 <gagan.dhand@non.se.com>
2020-05-19 18:29:28 +0530

    removing unnecessary code

--- SpringSecurityJwt/src/main/java/com/demo/SpringSecurityJwt/config/CorsFilter.java
+++ SpringSecurityJwt/src/main/java/com/demo/SpringSecurityJwt/config/CorsFilter.java
@@ -31,7 +31,7 @@ public class CorsFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
-				"Origin, X-Requested-With, Content-Type, Accept, Key, Authorization, thirdpartydevicepropertyid");
+				"Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
 
 		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
 			response.setStatus(HttpServletResponse.SC_OK);
